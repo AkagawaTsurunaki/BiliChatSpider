ab95e588d16622865bc0232dc0a273f6532fd366
AkagawaTsurunaki <965145619@qq.com>
2023-09-10 15:39:18 +0800

    feat: multitask_spider.py basically completed

--- core/multitask_spider.py
+++ core/multitask_spider.py
@@ -1,16 +1,13 @@
 import logging
-import random
-import time
-import traceback
+from multiprocessing import Process
 
 from retry import retry
 from selenium import webdriver
-from selenium.common import NoSuchElementException
-from selenium.webdriver.common.by import By
 
 from config import chat_spider_config as cfg
 from core.single_task_spider import SingleTaskSpider
 from dataset_manager import DatasetManager
+from history import History
 
 
 class MultitaskSpider:
@@ -18,10 +15,11 @@ class MultitaskSpider:
     def __init__(self, up_name: str, uid: str):
         self.up_name = up_name
         self.uid = uid
-        self.driver = self.__init_firefox_driver()
-        self.bili_spider = SingleTaskSpider(self.driver)
+        self.job = []
 
-    def __init_firefox_driver(self):
+    @staticmethod
+    @retry(Exception, tries=2)
+    def __get_driver():
         # 用户数据路径
         firefox_profile_dir = cfg.firefox_profile_dir
         # 实例化火狐设置选项
@@ -37,61 +35,35 @@ class MultitaskSpider:
 
         return driver
 
-    def __run(self):
-
-        diff_set = self.get_unsaved_video_list()
-
-        for bv in diff_set:
-            reply_content_list = self.bili_spider.get_reply_records(bv)
-            DatasetManager.save_task(up_name=self.up_name, bv=bv, uid=self.uid, data=reply_content_list)
-
-        self.driver.close()
-
-    # def get_unsaved_video_list(self):
-    #     video_list = self.get_video_list()
-    #     saved_video_list = self.dataset_manager.get_saved_video_list_by_space_id(self.uid)
-    #     diff_set = set(video_list) - set(saved_video_list)
-    #     return diff_set
-
-    @retry(NoSuchElementException, tries=2)
-    def get_video_list(self):
-        video_list = []
-        page_num = 1
-        while True:
-            url = f'https://space.bilibili.com/{self.uid}/video?tid=0&pn={page_num}&keyword=&order=pubdate'
-            self.driver.get(url)
-            self.driver.implicitly_wait(20)
-            # 减速, 防止触发验证码
-            time.sleep(random.Random().randint(a=1, b=2))
-            elems = self.driver.find_element(By.CLASS_NAME, 'list-list').find_elements(By.CLASS_NAME, 'list-item')
-            self.driver.implicitly_wait(20)
-            if len(elems) == 0: break
-            for elem in elems:
-                bv = elem.get_attribute('data-aid')
-                video_list.append(bv)
-            page_num += 1
-        self.driver.close()
-
-        return video_list
-
-    @retry(Exception, delay=4, tries=2)
-    def run_single(self, bv: str):
-
-        logging.debug(f'开始抓取“{bv}”的评论区评论区……')
-        reply_content_list = self.bili_spider.get_reply_records(bv)
-
-        self.dataset_manager.save(bv, self.uid, reply_content_list)
-        self.driver.close()
-        self.driver.quit()
+    @staticmethod
+    def __run_single_task(history: History, uid: str, bv: str):
+        logging.debug(f'Task {bv} executing...')
+        driver = MultitaskSpider.__get_driver()
+        data = SingleTaskSpider(driver).get_reply_records(bv)
+        DatasetManager().save_single_task(uid=uid, bv=bv, data=data)
+        history.single_task_completed(uid=uid, bv=bv)
+        driver.close()
+        driver.quit()
+        logging.debug(f'Task {bv} completed.')
 
     def run(self):
-        try:
-            logging.debug(f'蜘蛛大军即将启动。')
-            self.__run()
-            logging.debug('蜘蛛大军席卷完毕！')
-        except Exception as e:
-            traceback.print_exc()
-            logging.fatal('❌ 蜘蛛大军退出，因为遇到了未经处理的异常。')
-        finally:
-            self.driver.close()
-            self.driver.quit()
+
+        # If the job is completed then return straightly
+        history = History()
+        if history.is_job_completed(self.uid):
+            return
+
+        # If the job is not completed,
+        # Get the uncompleted bv list.
+        processes = []
+        for bv in history.get_uncompleted_tasks(self.uid):
+            process = Process(target=MultitaskSpider.__run_single_task, args=(history, self.uid, bv))
+            processes.append(process)
+
+        # Start all processes.
+        for process in processes:
+            process.start()
+
+        # Waiting for all processes completed.
+        for process in processes:
+            process.join()
