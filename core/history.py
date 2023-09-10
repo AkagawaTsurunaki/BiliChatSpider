b08c999771a9bcf545454a3d216cac645c1ede8c
AkagawaTsurunaki <965145619@qq.com>
2023-09-10 14:56:31 +0800

    feat: manager of program history

---
+++ core/history.py
@@ -0,0 +1,46 @@
+import json
+import os
+
+import config.chat_spider_config as cfg
+
+
+class History:
+
+    def __init__(self):
+        path = fr'{cfg.save_path}\history.json'
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        with open(file=path, encoding='utf-8', mode='r') as file:
+            self.history = json.load(fp=file)
+
+    @staticmethod
+    def is_job_completed(self, uid: str):
+        for history_item in self.history:
+            if history_item['uid'] == uid:
+                return history_item['is_job_completed']
+
+    @staticmethod
+    def get_uncompleted_tasks(self, uid: str):
+        for history_item in self.history:
+            if history_item['uid'] == uid:
+                return history_item['uncompleted_tasks']
+
+    @staticmethod
+    def single_task_completed(self, uid: str, bv: str):
+        for history_item in self.history:
+            if history_item['uid'] == uid:
+                # Remove bv from uncompleted_tasks
+                for uncompleted_task in history_item['uncompleted_tasks']:
+                    if uncompleted_task == bv:
+                        history_item['uncompleted_tasks'].remove(bv)
+                # Add bv into completed_tasks
+                history_item['completed_tasks'].append(bv)
+                # Check if all tasks completed
+                history_item['is_job_completed'] = len(history_item['uncompleted_tasks']) == 0
+        self.__save_history()
+
+    def __save_history(self):
+        with open(file=fr'{cfg.save_path}\history.json', encoding='utf-8', mode='r') as file:
+            json.dump(fp=file, ensure_ascii=False, obj=self.history)
