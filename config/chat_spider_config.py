e62bed8cd47c99977f85b6f2e8843d9d68526a33
AkagawaTsurunaki <965145619@qq.com>
2023-10-09 22:40:36 +0800

    refactor: max_parallel_job_num is set to 6

--- config/chat_spider_config.py
+++ config/chat_spider_config.py
@@ -1,9 +1,9 @@
-firefox_profile_dir = fr''
-firefox_driver_dir = fr''
+firefox_profile_dir = r""
+firefox_driver_dir = r""
 
-save_path = fr''
+save_path = r""
 
 sleep_time_before_job_launching = 4
 sleep_time_after_job_launching = 4
 
-max_parallel_job_num = 23
+max_parallel_job_num = 6
