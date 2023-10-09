4469d7c347289ee467d591d1d5892614130cef27
AkagawaTsurunaki <965145619@qq.com>
2023-10-09 17:08:23 +0800

    refactor: More readable config

--- config/chat_spider_config.py
+++ config/chat_spider_config.py
@@ -1,6 +1,9 @@
-max_parallel_job_num = 10
-save_path = r"..\dataset"
-firefox_profile_dir = r""
-firefox_driver_dir = r""
+firefox_profile_dir = fr''
+firefox_driver_dir = fr''
+
+save_path = fr''
+
 sleep_time_before_job_launching = 4
 sleep_time_after_job_launching = 4
+
+max_parallel_job_num = 23
