1c1ab53e3a053661cc4382e8bf580238b5b23f21
AkagawaTsurunaki <965145619@qq.com>
2023-10-09 17:57:55 +0800

    feat: Add log debug

--- core/driver_initilizer.py
+++ core/driver_initilizer.py
@@ -1,3 +1,4 @@
+import logging
 from retry import retry
 from selenium import webdriver
 
@@ -5,6 +6,10 @@ from config import chat_spider_config as cfg
 
 
 class DriverInitializer:
+
+    def __init__(self):
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
     @staticmethod
     @retry(Exception, tries=2)
     def get_firefox_driver():
@@ -15,8 +20,10 @@ class DriverInitializer:
         options = webdriver.FirefoxOptions()
         # 禁用加载图片
         options.set_preference('permissions.default.image', 2)
+        logging.debug(f'permissions.default.image is set to 2, images in the website will not be loaded.')
         # 禁用CSS
         options.set_preference('permissions.default.stylesheet', 2)
+        logging.debug(f'permissions.default.stylesheet is set to 2, css in the website will not be loaded.')
 
         options.profile = profile
         driver = webdriver.Firefox(options=options)
