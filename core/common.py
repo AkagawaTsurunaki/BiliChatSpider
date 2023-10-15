0efbabb467e8ff404fb64e157ef5d9b03bd7c52a
AkagawaTsurunaki <965145619@qq.com>
2023-10-15 20:18:31 +0800

    feat: scroll for xhs

--- core/common.py
+++ core/common.py
@@ -2,6 +2,8 @@ import logging
 import random
 import time
 
+from selenium.webdriver.firefox.webdriver import WebDriver
+
 
 def open_page(driver, url: str, wait_time=5):
     # Open specified page
@@ -20,3 +22,14 @@ def scroll(driver, offset, count=1):
         driver.execute_script(f'window.scrollBy(0,{offset})')
     logging.debug(
         f'🖱️ Total number of pixels of scrolling is {count * offset}. ')
+
+
+def xhs_scroll(driver: WebDriver, count=10, wait_time=1):
+    # Control the page to scroll
+    for _ in range(count):
+        js = """
+        const elem = document.querySelector('html body div#app div#global.layout.limit div.main-container div.with-side-bar.main-content div.outer-link-container div#noteContainer.note-container div.interaction-container div.note-scroller')
+        elem.scrollTop = elem.scrollHeight 
+        """
+        driver.execute_script(js)
+        time.sleep(wait_time)
--- xiaohongshu.py
+++ xiaohongshu.py
@@ -1,12 +1,43 @@
+import json
 import logging
 import time
+from typing import List
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
 from core.driver_initilizer import DriverInitializer
 
 from config import chat_spider_config as cfg
-from core.common import open_page, scroll
+from core.common import open_page, scroll, xhs_scroll
+
+
+class ReplyNode:
+    def __init__(self, username: str, content: str):
+        self.username: str = username
+        self.content: str = content
+        self.children: List[ReplyNode] = []
+
+    def to_dict(self):
+        return {
+            'username': self.username,
+            'content': self.content,
+            'children': [child.to_dict() for child in self.children]
+        }
+
+    def add(self, node):
+        if node is not None:
+            self.children.append(node)
+
+    def find(self, username):
+        if self.username == username:
+            return self
+
+        for node in self.children:
+            result = node.find(username)
+            if result is not None:
+                return result
+
+        return None
 
 
 class XhsSingleTaskSpider:
@@ -32,7 +63,7 @@ class XhsSingleTaskSpider:
 
     def collect(self, post_id: str):
         open_page(self.driver, f'https://www.xiaohongshu.com/explore/{post_id}')
-        scroll(self.driver, 1000)
+        xhs_scroll(self.driver, 1)
 
         # Start collect
         logging.debug(f'🗨️ Collecting replies from the page. ')
@@ -40,6 +71,7 @@ class XhsSingleTaskSpider:
 
 
 if __name__ == '__main__':
+
     print(cfg.xhs_save_path)
     driver = DriverInitializer.get_firefox_driver()
     XhsSingleTaskSpider(driver).collect('651280e60000000020000fa4')
