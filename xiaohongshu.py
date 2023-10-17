9639991c452321bd94a78a34b3894c86ec54620d
AkagawaTsurunaki <965145619@qq.com>
2023-10-17 16:38:09 +0800

    feat: collect from root replies

--- xiaohongshu.py
+++ xiaohongshu.py
@@ -3,6 +3,7 @@ import logging
 import time
 from typing import List
 
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.webdriver import WebDriver
 from core.driver_initilizer import DriverInitializer
@@ -45,33 +46,80 @@ class XhsSingleTaskSpider:
     def __init__(self, driver):
         self.driver: WebDriver = driver
 
-    def __collect(self, post_id: str):
+    def __get_root(self):
+        username_elem = self.driver.find_element(By.XPATH,
+                                                 '/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[1]/div/div[1]/a[2]')
+        title_elem = self.driver.find_element(By.XPATH, '//*[@id="detail-title"]')
+        detail_elem = self.driver.find_element(By.XPATH,
+                                               '/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[1]/div[2]/span[1]')
+        self.driver.implicitly_wait(1)
+        root = ReplyNode(username=username_elem.text, content=title_elem.text + detail_elem.text)
+        return root
 
-        # Show more comments
-        for i in range(1, 30):
-            root_comment_xpath = f'/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[{i}]'
-            root_comment_elem = self.driver.find_element(By.XPATH, root_comment_xpath)
+    def __deep(self, root: ReplyNode, i, j):
+        reply_name_xpath = f'/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[{i}]/div/div[2]/div[5]/div/div[{j}]/div/div[2]/div[1]/div/a'
+        reply_name_elem = self.driver.find_element(By.XPATH, reply_name_xpath)
 
+        reply_content_xpath = f'/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[{i}]/div/div[2]/div[5]/div/div[{j}]/div/div[2]/div[2]'
+        reply_content_elem = self.driver.find_element(By.XPATH, reply_content_xpath)
+
+        self.driver.implicitly_wait(1)
+
+        node = ReplyNode(username=reply_name_elem.text, content=reply_content_elem.text)
+        root.add(node)
+
+        return root
+
+    def show_more(self, comment_elem):
+        try:
             while True:
-                show_more_elem = root_comment_elem.find_element(By.CLASS_NAME, 'show-more')
+                show_more_elem = comment_elem.find_element(By.CLASS_NAME, 'show-more')
                 if show_more_elem is None:
                     break
-
                 show_more_elem.click()
                 # scroll(self.driver, count=10, offset=6000)
-                time.sleep(2)
+                time.sleep(1)
+        except NoSuchElementException:
+            pass
+
+    def __collect(self, root: ReplyNode):
+        # Show more comments
+        # TODO: Need more tests, remember change 2 to a var
+        for i in range(1, 2):
+            comment_xpath = f'/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[{i}]'
+            comment_content_xpath = f'/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[{i}]/div/div[2]/div[2]'
+            comment_name_xpath = f'/html/body/div[1]/div[1]/div[2]/div[2]/div/div[1]/div[3]/div[2]/div[2]/div/div[2]/div[{i}]/div/div[2]/div[1]/div/a'
+
+            comment_name_elem = self.driver.find_element(By.XPATH, comment_name_xpath)
+            comment_elem = self.driver.find_element(By.XPATH, comment_xpath)
+            comment_content_elem = self.driver.find_element(By.XPATH, comment_content_xpath)
+            self.driver.implicitly_wait(1)
+
+            comment = ReplyNode(username=comment_name_elem.text, content=comment_content_elem.text)
+            root.add(comment)
+
+            self.show_more(comment_elem)
+
+            # Deep Search
+            try:
+                for j in range(1, 100):
+                    comment = self.__deep(comment, i, j)
+            except NoSuchElementException:
+                pass
+
+            xhs_scroll(self.driver, 1)
+            print(root.to_dict())
 
     def collect(self, post_id: str):
         open_page(self.driver, f'https://www.xiaohongshu.com/explore/{post_id}')
-        xhs_scroll(self.driver, 1)
 
         # Start collect
         logging.debug(f'🗨️ Collecting replies from the page. ')
-        return self.__collect(post_id)
+        root = self.__get_root()
+        return self.__collect(root)
 
 
 if __name__ == '__main__':
-
     print(cfg.xhs_save_path)
     driver = DriverInitializer.get_firefox_driver()
     XhsSingleTaskSpider(driver).collect('651280e60000000020000fa4')
