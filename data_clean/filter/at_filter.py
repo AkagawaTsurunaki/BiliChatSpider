c2b2bf022bfe0a0539c616fdcca580f6667323e6
PermissionDog <455662503@qq.com>
2023-10-19 13:49:52 +0800

    feat: add filter which removes ats in the content.

---
+++ data_clean/filter/at_filter.py
@@ -0,0 +1,12 @@
+from data_structure import ReplyNode
+import re
+
+at_regex = re.compile('^@.+\s(.+)$')
+def at_filter(node: ReplyNode):
+    for child in node.children.copy():
+        result = at_regex.match(child.content)
+        if result is None:
+            node.children.remove(child)
+        else:
+            child.content = result.group(1)
+            at_filter(child)
