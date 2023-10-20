20b7efec3055c4a68402a5f4b78a868bf9cf2026
AkagawaTsurunaki <965145619@qq.com>
2023-10-20 23:14:37 +0800

    refactor: more tasks will be loaded by specified param

--- xiaohongshu/multitask_spider.py
+++ xiaohongshu/multitask_spider.py
@@ -49,24 +49,34 @@ def __load(cls: str, post_id_list: list):
     pool.join()
 
 
-def collect_by_channel_id(cls: str, channel_id: str):
-    url = f'https://www.xiaohongshu.com/explore?channel_id={channel_id}'
-    driver = DriverInitializer.get_firefox_driver(stylesheet=True)
-    open_page(driver, url, 0)
-    scroll(driver, 3000, 4)
+def __collect_by_channel_id(driver, channel_id: str):
+    post_id_set = []
+    try:
+        url = f'https://www.xiaohongshu.com/explore?channel_id={channel_id}'
+        open_page(driver, url, 0)
+        scroll(driver, 3000, 4, 1)
 
+        for i in range(1, 30):
+            post_id_xpath = f'/html/body/div[1]/div[1]/div[2]/div[2]/div/div[3]/section[{i}]/div/a[1]'
+            post_id_elem = driver.find_element(By.XPATH, post_id_xpath)
+            driver.implicitly_wait(2)
+            post_id_set.append(post_id_elem.get_attribute('href').split('/')[-1])
+
+        post_id_set = set(post_id_set) - get_xhs_saved_post_id_list()
+    except Exception:
+        pass
+
+    return post_id_set
+
+
+def collect_by_channel_id(cls: str, channel_id: str, task_count: int = 3):
+    driver = DriverInitializer.get_firefox_driver(stylesheet=True)
     post_id_list = []
-    for i in range(1, 10):
-        post_id_xpath = f'/html/body/div[1]/div[1]/div[2]/div[2]/div/div[3]/section[{i}]/div/a[1]'
-        # f'/html/body/div[1]/div[1]/div[2]/div[2]/div/div[3]/section[1]/div/div/a'
-        # f'/html/body/div[1]/div[1]/div[2]/div[2]/div/div[3]/section[2]/div/div/a'
-        driver.implicitly_wait(1)
-        post_id_elem = driver.find_element(By.XPATH, post_id_xpath)
-        post_id_list.append(post_id_elem.get_attribute('href').split('/')[-1])
+    for _ in range(task_count):
+        post_id_list += list(__collect_by_channel_id(driver, channel_id))
+        driver.refresh()
 
     driver.close()
     driver.quit()
-
-    post_id_list = set(post_id_list) - get_xhs_saved_post_id_list()
-
-    __load(cls, list(post_id_list))
+    print(f'Tasks: {len(post_id_list)}')
+    __load(cls, list(set(post_id_list)))
