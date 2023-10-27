958bb7a873101757c693f3144affcf9b13706abe
AkagawaTsurunaki <965145619@qq.com>
2023-10-27 18:00:01 +0800

    docs: translate Chinese to English

--- core/driver_initilizer.py
+++ core/driver_initilizer.py
@@ -13,16 +13,16 @@ class DriverInitializer:
     @staticmethod
     @retry(Exception, tries=2)
     def get_firefox_driver(image: bool = False, stylesheet: bool = False):
-        # 用户数据路径
+        # User data path
         firefox_profile_dir = cfg.firefox_profile_dir
-        # 实例化火狐设置选项
+        # Init webdriver option
         profile = webdriver.FirefoxProfile(firefox_profile_dir)
         options = webdriver.FirefoxOptions()
-        # 禁用加载图片
+        # Image permission
         if not image:
             options.set_preference('permissions.default.image', 2)
             logging.debug(f'permissions.default.image is set to 2, images in the website will not be loaded.')
-        # 禁用CSS
+        # Stylesheet permission
         if not stylesheet:
             options.set_preference('permissions.default.stylesheet', 2)
             logging.debug(f'permissions.default.stylesheet is set to 2, css in the website will not be loaded.')
