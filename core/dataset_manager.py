192744e1552f8baa23cc4f704014da3f51bec56d
AkagawaTsurunaki <965145619@qq.com>
2023-10-30 10:19:45 +0800

    refactor: __map, __load and exist functions are added.

--- core/dataset_manager.py
+++ core/dataset_manager.py
@@ -1,9 +1,10 @@
 import json
 import logging
 import os.path
+from typing import List, Set
 
 from config import chat_spider_config as cfg
-from core.common import open_json
+from core.common import open_json, save_json
 
 
 class DatasetManager:
@@ -11,9 +12,11 @@ class DatasetManager:
     def __init__(self):
         if not os.path.exists(cfg.save_path):
             os.makedirs(cfg.save_path)
+        if not os.path.exists(cfg.xhs_save_path):
+            os.makedirs(cfg.xhs_save_path)
 
     @staticmethod
-    def save_single_task(uid: str, bv: str, data):
+    def save_bili_single_task(uid: str, bv: str, data):
         # Prevent from raising exception if the folder is not created
         path = fr'{cfg.save_path}\{uid}'
         if not os.path.exists(path):
@@ -22,7 +25,8 @@ class DatasetManager:
         with open(file=fr'{path}\{bv}.json', mode='w', encoding='utf-8') as file:
             json.dump(obj=data, fp=file, ensure_ascii=False, indent=4)
 
-    def save_xhs_single_task(self, cls: str, page_id: str, data):
+    @staticmethod
+    def save_xhs_single_task(cls: str, page_id: str, data):
 
         # Prevent from raising exception if the folder is not created
         path = fr'{cfg.xhs_save_path}\{cls}'
@@ -30,67 +34,54 @@ class DatasetManager:
             os.makedirs(path)
         # Save the data by saving as json file
         path = fr'{path}\{page_id}.json'
-        if self.save_json(path, data):
+        if save_json(path, data):
             logging.debug(f'{path} is saved.')
-
-    @staticmethod
-    def save_json(path: str, obj):
-        try:
-            with open(file=path, mode='w', encoding='utf-8') as file:
-                json.dump(obj=obj, fp=file, ensure_ascii=False, indent=4)
-                return True
-        except IOError:
-            return False
-
-    @staticmethod
-    def print_statistic():
-        statistic = []
-        total = 0
-
-        if not os.path.exists(cfg.save_path):
-            raise FileNotFoundError('Save path is not found, please check your configuration file.')
-
-        for space_id in os.listdir(cfg.save_path):
-            path = fr'{cfg.save_path}\{space_id}'
-            if os.path.isdir(path):
-                data_sum = 0
-                for file_name in os.listdir(path):
-                    with open(f'{path}\\{file_name}', 'r', encoding='utf-8') as file:
-                        data_list = json.load(fp=file)
-                        data_sum += len(data_list)
-
-                statistic.append({
-                    'space_id': space_id,
-                    'num': data_sum
-                })
-                total += data_sum
-
-        print(f'Dataset Statistic Result')
-        index = 1
-        for item in statistic:
-            space_id = item['space_id']
-            num = item['num']
-            print(f'{index} ({space_id}): {num}')
-            index += 1
-        print(f'Total: {total}')
-        return statistic
-
-    @staticmethod
-    def __get_xhs_saved_file_name_list(path=cfg.xhs_save_path):
-        file_path_list = []
-        for dirpath, _, filenames in os.walk(path, topdown=True):
-            file_path_list += [os.path.join(dirpath, filename) for filename in filenames]
-        return list(set(file_path_list))
 
     @staticmethod
     def xhs_statistic():
         total = 0
-        for filepath in DatasetManager.__get_xhs_saved_file_name_list():
+        for filepath in DatasetManager.__load(cfg.xhs_save_path):
             root = open_json(filepath)
             total += len(root)
         return total
 
+    def bili_exist(self, bv) -> (bool, str | None):
+        return self.__exists(cfg.save_path, bv)
+
+    def xhs_exist(self, post_id) -> (bool, str | None):
+        return self.__exists(cfg.xhs_save_path, post_id)
+
     @staticmethod
-    def print_xhs_statistic():
-        print(f'Dataset (Xiaohongshu) Statistic Result')
-        print(f'Total: {DatasetManager.xhs_statistic()}')
+    def __exists(path: str, filename: str) -> (bool, str | None):
+        """
+        Check if the file with the specified filename already exists.
+        :param path: Dataset save path.
+        :param filename: Name of the file without its suffix.
+        :return: (True, fullpath) if file exists, (False, None) if file dose not exists.
+        """
+
+        for pair in DatasetManager.__map(path):
+            if pair['filename'] == filename:
+                return True, pair['fullpath']
+
+        return False, None
+
+    @staticmethod
+    def __map(path: str) -> List:
+        all_file = DatasetManager.__load(path)
+        result = list()
+        for full_file_path in all_file:
+            result += [{
+                'fullpath': full_file_path,
+                'filename': full_file_path.split('.')[-2]
+            }]
+        return result
+
+    @staticmethod
+    def __load(path: str) -> Set[str]:
+        result = set()
+        for dirpath, dirnames, filenames in os.walk(path):
+            for filename in filenames:
+                result.add(os.path.join(dirpath, filename))
+
+        return result
