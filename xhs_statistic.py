04dda37560c3c0a5955a8d692440ca4105e622a5
AkagawaTsurunaki <965145619@qq.com>
2023-10-30 10:18:47 +0800

    refactor: Statistic functions are transferred to bili_statistic.py and xhs_statistic.py separately.

--- xhs_statistic.py
+++ xhs_statistic.py
@@ -1,3 +1,10 @@
 from core.dataset_manager import DatasetManager
 
-DatasetManager.print_xhs_statistic()
+
+def print_xhs_statistic():
+    print(f'Dataset (Xiaohongshu) Statistic Result')
+    print(f'Total: {DatasetManager.xhs_statistic()}')
+
+
+if __name__ == '__main__':
+    print_xhs_statistic()
