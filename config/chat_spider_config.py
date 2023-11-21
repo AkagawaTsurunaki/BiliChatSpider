1e97396d6cdc5b7843e954430e1f09536d726246
AkagawaTsurunaki <965145619@qq.com>
2023-11-21 09:54:09 +0800

    feat: User-friendly config

--- config/chat_spider_config.py
+++ config/chat_spider_config.py
@@ -1,9 +1,14 @@
 firefox_profile_dir = r""
 firefox_driver_dir = r""
 
+# Bilibili Save Path
 save_path = r""
+
+# Xiaohongshu Save Path
+xhs_save_path = r""
 
 sleep_time_before_job_launching = 4
 sleep_time_after_job_launching = 4
 
-max_parallel_job_num = 6
+# The number of process
+max_parallel_job_num = 18
